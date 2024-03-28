7cf9b73ece65164889186582e6521ea467f2f232
sutanSultan <165270770+sutanSultan@users.noreply.github.com>
2024-03-28 16:17:05 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstNumber" },
+    { message: "enter second number", type: "number", name: "secondNumber" },
+    { message: "select one of the number to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplycation", "division"]
+    }
+]);
+if (answer.operator === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "multiplycation") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("please select valid number");
+}
